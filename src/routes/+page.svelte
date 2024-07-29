af76a6ebfa7a1e348a69ca84e294dbafbdb586a6
10x-dev-wannabe <dev10xattempt@yahoo.com>
2024-07-29 09:47:42 +0300

    Made basic svelte app template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<slot></slot>
+
+<h1>
+    Sports Timer
+</h1>
+
+<a href="/intervalTimer">Interval Timer</a><br>
+<a href="/reactionTimer">Reaction Timer</a>
---
+++ src/routes/reactionTimer/+page.svelte
@@ -0,0 +1,4 @@
+<h1>
+    Work in progress. Come back later.
+</h1>
+<a href="/">Back home</a>
