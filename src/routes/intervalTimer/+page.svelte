eb8c7a77e1984366666bc8b0078e63568f8e8971
10x-dev-wannabe <dev10xattempt@yahoo.com>
2024-07-29 20:32:52 +0300

    changed the inputs to a different menu in the interval timer

--- src/routes/intervalTimer/+page.svelte
+++ src/routes/intervalTimer/+page.svelte
@@ -1,53 +1,11 @@
-<script>
+<Menu />
 
+<script>
+    import Menu from "./menu.svelte";
 </script>
 
 
-<h1>
-    Interval Timer
-</h1>
-
-<div>
-    <table style="margin-left:auto; margin-right:auto; margin-bottom:5em">
-        <tr>
-            <td><h3>sets</h3></td>
-            <td><h3>work</h3></td>
-            <td><h3>rest</h3></td>
-        </tr><tr>
-            <td><input type="number" min="0" /></td>
-            <td><input type="time" min="0" /></td>
-            <td><input type="number" min="0" /></td>
-        </tr>
-    </table>
-</div>
-
-<button>
-    start
-</button>
-<br>
-
-<a href="/">Back home</a>
-
 <style>
-    tr, td{
-        padding: 1em
-    }
-    input[type="number"] {
-        text-align: center;
-        color: #fff;
-        font-size: 6em;
-        width: 4em;
-        background-color: #77777744;
-        border: none;
-        initial-letter: 0;
-    }
-    input[type="time"] {
-        text-align: center;
-        color: #fff;
-        font-size: 6em;
-        width: 4em;
-        background-color: #77777744;
-        border: none;
-        initial-letter: 0;
-    }
 </style>
+
+
---
+++ src/routes/intervalTimer/menu.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<a href="/" style="position: absolute; left:20px; top: 5px; z-index:1; text-decoration: none">&#8249<br></a>
+
+<div>
+    <table style="margin-left:auto; margin-right:auto; margin-top:1em">
+        <tr>sets</tr>
+        <tr><input type="number" min=0 max="100"></tr>
+        <tr>work</tr>
+        <tr><input type="number" min=0 max="100">:<input type="number" min=0 max="60"></tr>
+        <tr>rest</tr>
+        <tr><input type="number" min=0 max="100">:<input type="number" min=0 max="60"></tr>
+    </table>
+    
+</div>
+
+<style>
+    div {
+        position: absolute;
+        width: 20cm;
+        height: 90vh;
+        background-color: #55555555;
+        margin: 3em;
+        overflow: hidden;
+        text-align: center;
+    }
+    tr {
+        font-size: 5em;
+    }
+    input[type="number"] {
+        font-size: 1em;
+        text-align: center;
+        background-color: #ffffff11;
+        border-color: #ffffff11;
+        -webkit-appearance: textfield;
+        -moz-appearance: textfield;
+        appearance: textfield;
+        width: 6cm;
+    }
+</style>
