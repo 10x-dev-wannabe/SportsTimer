3b5e65f40ec97975d2b2fcaffe175b11ad4ff4b0
10x-dev-wannabe <dev10xattempt@yahoo.com>
2024-08-02 10:36:45 +0300

    moved the working page in the main page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,36 @@
     let sets
     let work
     let rest
-
+    let working = false;
+    function start() {
+        working = true;
+    }
+    function stop() {
+        working = false;
+    }
 </script>
 
 <main>
 
+{#if working}
+    <button id="stop" on:click={stop}>
+        <div style="height: 0;">
+            <p style="margin: 0; padding-top:0; line-height:1px">
+                &#10799;
+            </p>
+        </div>
+    </button>
+{:else}
     <Menu 
         bind:sets={sets}
         bind:work={work}
         bind:rest={rest}
     />
 
-    <a href="/working">
-        <button id='start'>
-            START
-        </button>
-    </a>
+    <button id='start' on:click={start}>
+        START
+    </button>
+{/if}
 
 </main>
 
@@ -26,7 +40,18 @@
     #start {
         background-color: #00ff00;
         position: absolute;
-        bottom: 4vh;
-        right: 4vh;
+        bottom: 4vmin;
+        right: 4vmin;
+    }
+    #stop {
+        position: absolute;
+        top: 4vmin;
+        left: 4vmin;
+        padding: none;
+        background-color: #00000000;
+        width: auto;
+        height: auto;
+        font-size: 10em;
+        border: none;
     }
 </style>
